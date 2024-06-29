fc94506858ac6d876a0f06d053054061f23eb6d4
Thiago <thiagovpaz@MacBook-Pro-de-Thiago.local>
2024-06-29 20:22:18 -0300

    feat: add signin and signup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div />
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+
+  return <div>{JSON.stringify(user, null, 2)}</div>
 }
